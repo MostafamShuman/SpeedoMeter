9402a43cbfe19fc5908f47be1ae7c9e6ba27f8c3
mostafa shuman <>
2018-08-24 23:01:35 +0200

    Location Speed Provider

--- SpeedoMeter/Location/SPDLocationSpeedProvider.swift
+++ SpeedoMeter/Location/SPDLocationSpeedProvider.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2018 Mostafa Shuman. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+protocol SPDLocationSpeedProviderDelegate: class {
+    func didUpdate(speed: CLLocationSpeed)
+}
+protocol SPDLocationSpeedProvider: class {
+    var delegate: SPDLocationSpeedProviderDelegate? { get set }
+}
+class SPDDefaultLocationSpeedProvider {
+    let locationProvider: SPDLocationProvider
+    weak var delegate: SPDLocationSpeedProviderDelegate?
+    init(locationProvider: SPDLocationProvider) {
+        self.locationProvider = locationProvider
+        locationProvider.add(self)
+    }
+}
+extension SPDDefaultLocationSpeedProvider: SPDLocationSpeedProvider {
+    
+}
+extension SPDDefaultLocationSpeedProvider: SPDLocationConsumer {
+    func consumeLocation(_ location: CLLocation) {
+        let speed = max(location.speed, 0)
+        delegate?.didUpdate(speed: speed)
+    }
+    
+}
