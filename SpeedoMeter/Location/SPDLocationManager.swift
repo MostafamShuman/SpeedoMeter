12d62bea358768292a5554b870326a4d0c6e4696
mostafa shuman <>
2018-08-24 22:47:40 +0200

    first commit

---
+++ SpeedoMeter/Location/SPDLocationManager.swift
@@ -0,0 +1,9 @@
+//
+//  SPDLocationManager.swift
+//  SpeedoMeter
+//
+//  Created by Mostafa Shuman on 8/16/18.
+//  Copyright © 2018 Mostafa Shuman. All rights reserved.
+//
+
+import Foundation
---
+++ SpeedoMeter/Location/SPDLocationProvider.swift
@@ -0,0 +1,9 @@
+//
+//  SPDLocationProvider.swift
+//  SpeedoMeter
+//
+//  Created by Mostafa Shuman on 8/24/18.
+//  Copyright © 2018 Mostafa Shuman. All rights reserved.
+//
+
+import Foundation
